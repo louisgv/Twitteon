146d2ae7729803cf888ab56c8e5f5e248471c856
Louis Graham Vichy <lgvichy@gmail.com>
2014-04-20 14:11:00 -0700

    Initialization

---
+++ js/index.js
@@ -0,0 +1,2 @@
+//This script will check if each slide is on, it also help in sliding the canvas so that they fit.
+// Each canvas will have some states. And for each canvas, if they are show in full, this script will load the other.
